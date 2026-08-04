8464aea3ef718b3a6e0777dfe556b993bcba79f3
keyzaw <keyzaalbian413@gmail.com>
2026-08-04 22:46:36 +0700

    Add files via upload

---
+++ VIDEO/DASAR/Percabangan/Percabangan/Program.cs
@@ -0,0 +1,50 @@
+﻿// IF
+int umur = 15;
+//if (umur >= 17)
+//{
+//    Console.WriteLine("Boleh membuat KTP");
+//}
+
+// ELSE
+if (umur >= 17)
+{
+    Console.WriteLine("Boleh membuat KTP");
+}
+else
+{
+    Console.WriteLine("Belum boleh membuat KTP");
+}
+
+// ELSE IF
+int nilai = 85;
+
+if (nilai >= 90)
+{
+    Console.WriteLine("Grade A");
+
+} else if (nilai >= 70) {
+    Console.WriteLine("Grade B");
+
+}
+else {
+    Console.WriteLine("Grade C");
+
+}
+
+// Switch
+String hari = "Rabu";
+
+switch (hari)
+{
+    case "Senin":
+        Console.WriteLine("Hari Pertama");
+        break;
+
+    case "Minggu":
+        Console.WriteLine("Hari Ketujuh");
+        break;
+
+    default:
+        Console.WriteLine("Hari Lain");
+        break;
+}
