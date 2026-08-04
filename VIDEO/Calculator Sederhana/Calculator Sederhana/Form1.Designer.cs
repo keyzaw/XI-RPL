dc6621aebbfa9992d7363a40a078ae6d2a6f169f
keyzaw <keyzaalbian413@gmail.com>
2026-08-04 22:42:59 +0700

    Add files via upload

---
+++ VIDEO/Calculator Sederhana/Calculator Sederhana/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace Calculator_Sederhana
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAngka1 = new System.Windows.Forms.TextBox();
+            this.btnTambah = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtAngka2 = new System.Windows.Forms.TextBox();
+            this.lbHasil = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnKurang = new System.Windows.Forms.Button();
+            this.btnKali = new System.Windows.Forms.Button();
+            this.btnBagi = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Angka 1";
+            this.label1.UseMnemonic = false;
+            // 
+            // txtAngka1
+            // 
+            this.txtAngka1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAngka1.Location = new System.Drawing.Point(12, 71);
+            this.txtAngka1.Name = "txtAngka1";
+            this.txtAngka1.Size = new System.Drawing.Size(328, 25);
+            this.txtAngka1.TabIndex = 1;
+            // 
+            // btnTambah
+            // 
+            this.btnTambah.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTambah.Location = new System.Drawing.Point(12, 251);
+            this.btnTambah.Name = "btnTambah";
+            this.btnTambah.Size = new System.Drawing.Size(328, 33);
+            this.btnTambah.TabIndex = 2;
+            this.btnTambah.Text = "Tambah";
+            this.btnTambah.UseVisualStyleBackColor = true;
+            this.btnTambah.Click += new System.EventHandler(this.btnTambah_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(9, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Angka 2";
+            this.label2.UseMnemonic = false;
+            // 
+            // txtAngka2
+            // 
+            this.txtAngka2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAngka2.Location = new System.Drawing.Point(12, 135);
+            this.txtAngka2.Name = "txtAngka2";
+            this.txtAngka2.Size = new System.Drawing.Size(328, 25);
+            this.txtAngka2.TabIndex = 1;
+            // 
+            // lbHasil
+            // 
+            this.lbHasil.AutoSize = true;
+            this.lbHasil.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbHasil.Location = new System.Drawing.Point(9, 182);
+            this.lbHasil.Name = "lbHasil";
+            this.lbHasil.Size = new System.Drawing.Size(47, 17);
+            this.lbHasil.TabIndex = 0;
+            this.lbHasil.Text = "Hasil :";
+            this.lbHasil.UseMnemonic = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(155, 231);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 17);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Aksi";
+            this.label3.UseMnemonic = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(73, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(201, 25);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Calculator Sederhana";
+            this.label4.UseMnemonic = false;
+            // 
+            // btnKurang
+            // 
+            this.btnKurang.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnKurang.Location = new System.Drawing.Point(12, 290);
+            this.btnKurang.Name = "btnKurang";
+            this.btnKurang.Size = new System.Drawing.Size(328, 33);
+            this.btnKurang.TabIndex = 2;
+            this.btnKurang.Text = "Kurang";
+            this.btnKurang.UseVisualStyleBackColor = true;
+            this.btnKurang.Click += new System.EventHandler(this.btnKurang_Click);
+            // 
+            // btnKali
+            // 
+            this.btnKali.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnKali.Location = new System.Drawing.Point(12, 329);
+            this.btnKali.Name = "btnKali";
+            this.btnKali.Size = new System.Drawing.Size(328, 33);
+            this.btnKali.TabIndex = 2;
+            this.btnKali.Text = "Kali";
+            this.btnKali.UseVisualStyleBackColor = true;
+            this.btnKali.Click += new System.EventHandler(this.btnKali_Click);
+            // 
+            // btnBagi
+            // 
+            this.btnBagi.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBagi.Location = new System.Drawing.Point(12, 368);
+            this.btnBagi.Name = "btnBagi";
+            this.btnBagi.Size = new System.Drawing.Size(328, 33);
+            this.btnBagi.TabIndex = 2;
+            this.btnBagi.Text = "Bagi";
+            this.btnBagi.UseVisualStyleBackColor = true;
+            this.btnBagi.Click += new System.EventHandler(this.btnBagi_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(352, 415);
+            this.Controls.Add(this.btnBagi);
+            this.Controls.Add(this.btnKali);
+            this.Controls.Add(this.btnKurang);
+            this.Controls.Add(this.btnTambah);
+            this.Controls.Add(this.txtAngka2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbHasil);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtAngka1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAngka1;
+        private System.Windows.Forms.Button btnTambah;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtAngka2;
+        private System.Windows.Forms.Label lbHasil;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnKurang;
+        private System.Windows.Forms.Button btnKali;
+        private System.Windows.Forms.Button btnBagi;
+    }
+}
+
---
+++ VIDEO/DASAR/ArrayDanKoleksiDasar/ArrayDanKoleksiDasar/Program.cs
@@ -0,0 +1,27 @@
+﻿// Array
+string[] namaBuah = { "Apel", "Mangga", "Jeruk" };
+
+// Mengambil Data Array
+//Console.WriteLine(namaBuah[0]);
+//Console.WriteLine(namaBuah[2]);
+
+// Array juga seringndigunakan bersama dengan perulangan foreach
+foreach (string buah in namaBuah)
+{
+    Console.WriteLine(buah);
+}
+
+// koleksi dasar : list
+
+List<string> namaSiswa = new List<string>();
+
+// Menambah data ke list
+namaSiswa.Add("Budi");
+namaSiswa.Add("Anton");
+namaSiswa.Add("Siti");
+
+// Menampilkan hasil dari list
+foreach (string siswa in namaSiswa)
+{
+    Console.WriteLine(siswa);
+}
